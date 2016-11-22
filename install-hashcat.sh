2ed9842528f7d6ec669ae76797321d68f94a64e1
Alexandre Bezroutchko <abb@gremwell.com>
2016-11-22 19:18:59 +0000

    install scripts

---
+++ install-hashcat.sh
@@ -0,0 +1,12 @@
+!/bin/sh -x
+
+cd /tmp
+
+apt-get install -y build-essential git
+
+git clone https://github.com/hashcat/hashcat.git
+
+(cd hashcat/ && git submodule init  && git submodule update && make)
+
+./hashcat/hashcat -b
+
---
+++ install-opencl.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+cd /tmp
+
+apt-get update
+apt-get install -y lsb-core
+
+wget http://registrationcenter-download.intel.com/akdlm/irc_nas/9019/opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25.tgz
+tar xfz opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25.tgz 
+(cd opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25 && sudo ./install.sh)
+
+rm -rf opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25 opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25.tgz
+
